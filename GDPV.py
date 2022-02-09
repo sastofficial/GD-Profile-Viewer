896e486c6a43e524c31d1cdb3f0224ba27061ee7
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-02-09 17:10:35 +0100

    Expanded the template + started on the app

--- GDPV.py
+++ GDPV.py
@@ -1,2 +1,5 @@
 # GDPV 1.0 by SA ST
 from requests import get
+input("Welcome to GDPV! Press enter to continue!\n")
+gd_player = input("Input a username or user id\n")
+player_info = get("https://gdbrowser.com/api/profile/"+gd_player)
